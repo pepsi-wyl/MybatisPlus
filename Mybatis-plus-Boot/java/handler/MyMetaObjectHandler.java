90402f214b2933e0ff77454da54243f1e84602c4
wyl <82953346+pepsi-wyl@users.noreply.github.com>
2021-11-29 16:08:38 +0800

    Create MyMetaObjectHandler.java

---
+++ Mybatis-plus-Boot/java/handler/MyMetaObjectHandler.java
@@ -0,0 +1,32 @@
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+/**
+ * @author by wyl
+ * @date 2021/11/28.20点57分
+ */
+
+@Slf4j
+@Component   //处理器加入IOC容器中
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    //插入时候的填充策略
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("start insert fill ....");
+        this.setFieldValByName("createTime", new Date(), metaObject);
+    }
+
+    //更新时候的填充策略
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("start update fill ....");
+        this.setFieldValByName("updateTime", new Date(), metaObject);
+    }
+
+}
